c00a5eabd9073385fba9b7717706e5932cdd7094
Terrell Cooper <TerrellCoop@gmail.com>
2020-12-22 16:16:34 -0700

    start of weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+
+const api = {
+  key: "56da9185963cb122e472cf80b61488b7", 
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app warm">
+      <main>
+        <div className='search-box'>
+          <input
+            type='text'
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+      </main>
     </div>
   );
 }
